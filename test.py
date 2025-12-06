e7b2c17ea217a6e374c816c6b057afd797364e54
Sawyer Bowerman <117670150+shplok@users.noreply.github.com>
2025-12-05 19:40:46 -0500

    revert silliness

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from src.ev_tracker import quick_analyze
 from src.ev_tracker import EVTracker
 
 tracker = EVTracker()
-results = tracker.run_batch(
+results = tracker.run(
     tiff_file=r"c:\Users\sawye\OneDrive - University of Massachusetts Boston\Desktop\xslot_HCC1954_01_500uLhr_z40um_mov_1_MMStack_Pos0.ome.tif",
     ground_truth_csv=r"c:\Users\sawye\OneDrive - University of Massachusetts Boston\Desktop\xslot_HCC1954_01_500uLhr_z40um_mov_1.csv"
 )
